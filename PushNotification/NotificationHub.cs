fd45d6bb18fde7fe9bb51c7a1dbda274393b39b2
Geo <Geo@DESKTOP-4HDFJN5>
2020-09-05 18:31:53 +0300

    Add project files.

---
+++ PushNotification/NotificationHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace PushNotification
+{
+    //SELECT name, is_broker_enabled FROM sys.databases //
+    public class NotificationHub : Hub
+    {
+       // public void Hello()
+       // {
+      //      Clients.All.hello();
+       // }
+    }
+}
